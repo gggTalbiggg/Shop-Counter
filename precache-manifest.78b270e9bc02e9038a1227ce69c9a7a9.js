7ef4dfdbebe597f8985869b471b3c32283f93619
gggTalbiggg <ga_talbi@esi.dz>
2020-07-07 17:56:21 +0100

    Updates

---
+++ precache-manifest.78b270e9bc02e9038a1227ce69c9a7a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1801a05a3a1b4f36caf529e4c40350e",
+    "url": "/Shop-Counter/index.html"
+  },
+  {
+    "revision": "36764db0be3a2e1efddc",
+    "url": "/Shop-Counter/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "07384238e7a2e8baaca2",
+    "url": "/Shop-Counter/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "36764db0be3a2e1efddc",
+    "url": "/Shop-Counter/static/js/2.966a6480.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Shop-Counter/static/js/2.966a6480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07384238e7a2e8baaca2",
+    "url": "/Shop-Counter/static/js/main.abcfab29.chunk.js"
+  },
+  {
+    "revision": "7f428c1f8e7b180d6a18",
+    "url": "/Shop-Counter/static/js/runtime-main.53a1f471.js"
+  }
+]);
